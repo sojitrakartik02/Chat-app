fff15ca5b4d89d962a4f091d7ba39250c20ce0d0
Kartikninja <kartikninjatechnolabs@gmail.com>
2025-02-02 12:23:39 +0530

    change in tailwing.confige

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,10 @@
+// /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 
 export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [daisyui],
 };
-
-// /** @type {import('tailwindcss').Config} */
-// export default {
-// 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-// 	theme: {
-// 		extend: {},
-// 	},
-// 	// eslint-disable-next-line no-undef
-// 	plugins: [require("daisyui")],
-// };
